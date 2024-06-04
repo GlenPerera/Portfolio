561b4a73b74840b3bc482d432642a7c20baeeb21
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-06-04 10:18:14 +0530

    added the updated CV

--- src/app/CV/page.tsx
+++ src/app/CV/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cv = () => {
   return (
     <div>
-      <iframe src="/CV (2).pdf" width="100%" height="800px" title="CV"></iframe>
+      <iframe src="/GlenPerera-CS.pdf" width="100%" height="800px" title="CV"></iframe>
     </div>
   );
 };
